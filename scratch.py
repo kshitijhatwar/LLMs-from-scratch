6265269270fe77cdaddb44f321fb1f6a19b6d128
Kshitij Hatwar <99862816+kshitijhatwar@users.noreply.github.com>
2025-10-30 22:47:27 +0530

    edited tokenizer

--- scratch.py
+++ scratch.py
@@ -30,16 +30,17 @@ vocab = {token:integer for integer,token in enumerate(all_words)}
 
 #########
 
-class SimpleTokenizerV1:
+class SimpleTokenizer:
     def __init__(self, vocab):
         self.str_to_int = vocab
         self.int_to_str = {i:s for s,i in vocab.items()}
     
     def encode(self, text):
-        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
-                                
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)                      
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
         preprocessed = [
-            item.strip() for item in preprocessed if item.strip()
+            item if item in self.str_to_int 
+            else "<|unk|>" for item in preprocessed
         ]
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
