78b6860cdf34cce5f12d15e0c3e9d015c5241007
Kshitij Hatwar <99862816+kshitijhatwar@users.noreply.github.com>
2025-10-31 22:19:41 +0530

    code update

--- Project2/llm_train.py
+++ Project2/llm_train.py
@@ -1,19 +1,77 @@
-
+###########################Using BYTE PAIR ENCODING (BPE)
+##########################
 import importlib
 import tiktoken
 
-
 tokenizer = tiktoken.get_encoding("gpt2")
+###############################################################
+####################################################
+from torch.utils.data import Dataset, DataLoader
 
-text = (
-    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
-     "of someunknownPlace."
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        # Tokenize the entire text
+        token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
+
+        # Use a sliding window to chunk the book into overlapping sequences of max_length
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+###################################### Creating DataLoader
+#####################################
+
+def create_dataloader_v1(txt, batch_size=4, max_length=256,
+                         stride=128, shuffle=True, drop_last=True,
+                         num_workers=0):
+
+    # Initialize the tokenizer
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    # Create dataset
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+
+    # Create dataloader
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
+
+
+##########################################################
+##############################################
+
+with open("book.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+#################################################
+############################################3
+
+import torch
+print("PyTorch version:", torch.__version__)
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=1, max_length=4, stride=1, shuffle=False
 )
 
-integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
 
-print(integers)
-
-strings = tokenizer.decode(integers)
-
-print(strings)
+second_batch = next(data_iter)
+print(second_batch)
